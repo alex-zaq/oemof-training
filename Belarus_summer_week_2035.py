b839348e04acf3c5c7497358cb0f8398bc48d53b
AlexeyZaq <melnikov1991@bk.ru>
2022-04-08 16:53:02 +0300

    add_slicing_for_period

--- Belarus_summer_week_2035.py
+++ Belarus_summer_week_2035.py
@@ -11,25 +11,31 @@ import seaborn as sns
 ##################################################
 number_of_time_steps = 24 * 7
 current_folder = os.getcwd()
-demands_power = pd.read_excel(os.path.join(current_folder,'demands_power_summer.xlsx'))
-tech_fix = pd.read_excel(os.path.join(current_folder,"Tech_Fix _summer.xlsx"))
-tech_max = pd.read_excel(os.path.join(current_folder,"Tech_max_summer.xlsx"))
-
-
+demands_power = pd.read_excel(os.path.join(current_folder,'demands_power.xlsx'))
+tech_fix = pd.read_excel(os.path.join(current_folder,"Tech_Fix.xlsx"))
+tech_max = pd.read_excel(os.path.join(current_folder,"Tech_max.xlsx"))
 date_time_index = pd.date_range("06/11/2035",periods=number_of_time_steps,freq="H")
 energysystem = solph.EnergySystem(timeindex=date_time_index)
 ##################################################
 
 
+# Period options
+###################################################
+summer_week_slice = slice(3768,3936)
 
-# options
+demands_power = demands_power[summer_week_slice].reset_index(drop=True)
+tech_fix = tech_fix[summer_week_slice].reset_index(drop=True)
+tech_max = tech_max[summer_week_slice].reset_index(drop=True)
+###################################################
+
+
+# NPP options
 ##################################################
 Bel_NPP_fix_lst = ["Bel_NPP_July_December","Bel_NPP_April_December","Full_Power"]
 NEW_NPP_TOI_fix_lst = ["New_NPP_June","New_NPP_February","Full_Power"]
 Bel_NPP_fix = tech_fix[Bel_NPP_fix_lst[2]]
 NEW_NPP_Toi = tech_fix[NEW_NPP_TOI_fix_lst[2]]
 ###################################################
-
 
 
 # Fuels

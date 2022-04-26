23b7c4e5643f246eaa1d948993fc1c43eb4f2c55
AlexeyZaq <melnikov1991@bk.ru>
2022-04-26 15:00:19 +0300

    step

--- Belarus_8760_2035.py
+++ Belarus_8760_2035.py
@@ -20,7 +20,6 @@ energysystem = solph.EnergySystem(timeindex=date_time_index)
 
 
 
-
 # options
 ##################################################
 Bel_NPP_fix_lst = ["Bel_NPP_July_December","Bel_NPP_April_December","Full_Power"]

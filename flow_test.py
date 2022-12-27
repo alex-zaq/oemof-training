e7f850888f83d837df6382393b273ec4a547fa7a
AlexeyZaq <melnikov1991@bk.ru>
2022-12-27 17:29:48 +0300

    step

--- flow_test.py
+++ flow_test.py
@@ -11,20 +11,19 @@ from oemof_visio import ESGraphRenderer
 
 number_of_time_steps = 24
 current_start_date = dt.datetime(2020,6,8,1,0,0)
-date_time_index = pd.date_5range(current_start_date, periods=number_of_time_steps, freq="H")
+date_time_index = pd.date_range(current_start_date, periods=number_of_time_steps, freq="H")
 energysystem = solph.EnergySystem(timeindex=date_time_index, infer_last_interval= False)
 
 
-current_folder = os.getcwd()
+current_folder = os.getcwd() + '/data'
 el_global_abs_profile = pd.read_excel(os.path.join(current_folder,'data_by_day.xlsx'), sheet_name='electric_demand_2021_odu')['el_winter_workDay_odu'][:number_of_time_steps]
-heat_demand_data = [1000] * 24;
+heat_demand_data = [3000] * 24;
 
 
 
 natural_gas_global_bus = solph.Bus(label = "natural_gas_global_bus" )
 electricity_bus = solph.Bus(label = "electricity_bus" )
 heat_water_bus = solph.Bus(label = "heat_water_bus" )
-# steam_bus = solph.Bus(label = "steam_bus" )
 energysystem.add(natural_gas_global_bus, electricity_bus, heat_water_bus)
 
 
@@ -33,9 +32,7 @@ natural_gas_generator_source = solph.components.Source (
 			outputs = {natural_gas_global_bus: solph.Flow(variable_costs = 0)} 
    )
 energysystem.add(natural_gas_generator_source) 
- 
- 
-  
+   
 back_electricity_tr = solph.components.Transformer (
     label = 'back_electricity_tr',
 		inputs = {natural_gas_global_bus: solph.Flow()},
@@ -44,8 +41,7 @@ back_electricity_tr = solph.components.Transformer (
 		
  ) 
 energysystem.add(back_electricity_tr)
- 
-   
+    
 back_heat_water_tr = solph.components.Transformer (
     label = 'back_heat_water_tr',
 		inputs = {natural_gas_global_bus: solph.Flow()},
@@ -55,28 +51,49 @@ energysystem.add(back_heat_water_tr)
  
  
 
-chp_natural_gas_bus = solph.Bus('chp_natural_gas_bus')
-energysystem.add(chp_natural_gas_bus)
+# chp_natural_gas_bus = solph.Bus('chp_natural_gas_bus')
+# energysystem.add(chp_natural_gas_bus)
+#  chp_natural_gas_tr = solph.components.Transformer (
+#     label = 'chp_natural_gas_tr',
+# 		inputs = {natural_gas_global_bus: solph.Flow()},
+# 		outputs = {chp_natural_gas_bus: solph.Flow(nominal_value = 2000)},
+#  ) 
+# energysystem.add(chp_natural_gas_tr)
  
-chp_natural_gas_tr = solph.components.Transformer (
-    label = 'chp_natural_gas_tr',
+ 
+electricity_chp_bus = solph.Bus(label = "electricity_chp_bus" )
+energysystem.add(electricity_chp_bus)
+     
+ 
+chp_electricity_tr_first = solph.components.Transformer (
+    label = 'chp_electricity_tr_first',
 		inputs = {natural_gas_global_bus: solph.Flow()},
-		outputs = {chp_natural_gas_bus: solph.Flow(nominal_value = 2000)},
- ) 
-energysystem.add(chp_natural_gas_tr)
- 
-    
- 
-back_chp_electricity_tr = solph.components.Transformer (
-    label = 'back_chp_electricity_tr',
-		inputs = {chp_natural_gas_bus: solph.Flow()},
-		outputs = {electricity_bus: solph.Flow(nominal_value = 500, variable_costs = 400, min = 0.2, nonconvex = solph.NonConvex()), heat_water_bus: solph.Flow()},
-		conversion_factors = {chp_natural_gas_bus:4,  electricity_bus:1, heat_water_bus:2}
+		outputs = {electricity_chp_bus: solph.Flow(nominal_value = 1000, variable_costs = -1000),
+             	heat_water_bus: solph.Flow(variable_costs = -1000)},
+		conversion_factors = {natural_gas_global_bus:4,  electricity_chp_bus:1, heat_water_bus:2}
 		
  ) 
-energysystem.add(back_chp_electricity_tr)
+energysystem.add(chp_electricity_tr_first)
+   
+   
+   
+    
+chp_electricity_tr_second = solph.components.Transformer (
+    label = 'chp_electricity_tr_second',
+		inputs = {electricity_chp_bus: solph.Flow()},
+		outputs = {electricity_bus: solph.Flow(nominal_value = 50, min = 0.1, nonconvex = solph.NonConvex()),
+             	},
+		conversion_factors = {electricity_chp_bus:1,  electricity_bus:1 }
+		
+ ) 
+energysystem.add(chp_electricity_tr_second)
  
  
+  
+   
+   
+   
+   
    
    
 electricity_sink = solph.components.Sink(
@@ -86,7 +103,6 @@ electricity_sink = solph.components.Sink(
    	nominal_value = 1)}
 
    )
-
    
 heat_water_sink = solph.components.Sink(
 		label = 'heat_water_sink',
@@ -107,19 +123,19 @@ results = solph.processing.results(model)
 # print(results)
 
 electricity_data = solph.views.node(results, "electricity_bus")["sequences"].dropna()
-chp_gas_data = solph.views.node(results, "chp_natural_gas_bus")["sequences"].dropna()
+gas_data = solph.views.node(results, "natural_gas_global_bus")["sequences"].dropna()
 heat_water_data = solph.views.node(results, "heat_water_bus")["sequences"].dropna()
 
 
-electr_df = pd.DataFrame()
-electr_df['ТЭЦ-электроэнергия'] = electricity_data[((back_chp_electricity_tr.label, electricity_bus.label),'flow')]
+# electr_df = pd.DataFrame()
+# electr_df['ТЭЦ-электроэнергия'] = electricity_data[((chp_electricity_tr_first.label, electricity_bus.label),'flow')]
 # electr_df['Дорогой генератор'] = electricity_data[((back_electricity_tr.label, electricity_bus.label),'flow')]
 
 gas_df = pd.DataFrame()
-gas_df['Расход газа ТЭЦ'] = chp_gas_data[((chp_natural_gas_bus.label, back_chp_electricity_tr.label),'flow')]
+gas_df['Расход газа ТЭЦ'] = gas_data[((natural_gas_global_bus.label, chp_electricity_tr_first.label),'flow')]
 
 heat_water_df = pd.DataFrame()
-heat_water_df['ТЭЦ-горячая вода'] = heat_water_data[((back_chp_electricity_tr.label, heat_water_bus.label),'flow')]
+heat_water_df['ТЭЦ-горячая вода'] = heat_water_data[((chp_electricity_tr_first.label, heat_water_bus.label),'flow')]
 
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
@@ -128,12 +144,12 @@ fig, axes = plt.subplots(nrows=2, ncols=2)
 
 
 
-ax1 = electr_df.plot(kind="area", ylim=(0, 3000), ax = axes[0,1]  ,legend = 'reverse', title = 'Генерация электроэнергии')
+# ax1 = electr_df.plot(kind="area", ylim=(0, 3000), ax = axes[0,1]  ,legend = 'reverse', title = 'Генерация электроэнергии')
 ax2 = heat_water_df.plot(kind="area", ylim=(0, 3000) , ax = axes[1,0] ,  legend = 'reverse', title = 'Генерация тепла')
 ax3 = gas_df.plot(kind="area", ylim=(0, 3000) , ax = axes[0, 0] ,  legend = 'reverse', title = 'Расход газа ТЭЦ')
 
 
-gr = ESGraphRenderer(energy_system=energysystem, filepath="energy_system", img_format="png", txt_fontsize=10, txt_width=10)
-gr.view()
+# gr = ESGraphRenderer(energy_system=energysystem, filepath="energy_system", img_format="png", txt_fontsize=10, txt_width=10)
+# gr.view()
 
 plt.show()
--- pt_turbine_model.py
+++ pt_turbine_model.py
@@ -16,8 +16,8 @@ energysystem = solph.EnergySystem(timeindex=date_time_index, infer_last_interval
 
 data_folder = os.getcwd()+'/data'
 el_global_abs_profile = pd.read_excel(os.path.join(data_folder,'data_by_day.xlsx'), sheet_name='electric_demand_2021_odu')['el_winter_workDay_odu'][:number_of_time_steps]
-heat_demand_data = [400] * 24;
-steam_demand_data = [600] * 24;
+heat_demand_data = [3000] * 24;
+steam_demand_data = [3000] * 24;
 
 
 ##########################################################################################################################
@@ -25,113 +25,123 @@ natural_gas_global_bus = solph.Bus(label = "natural_gas_global_bus" )
 electricity_bus = solph.Bus(label = "electricity_bus" )
 heat_water_bus = solph.Bus(label = "heat_water_bus" )
 steam_bus = solph.Bus(label = "steam_bus" )
-energysystem.add(natural_gas_global_bus, electricity_bus, steam_bus)
+energysystem.add(natural_gas_global_bus, electricity_bus, heat_water_bus, steam_bus)
 ##########################################################################################################################
 
 natural_gas_generator_source = solph.components.Source ( 
 			label = 'natural_gas_generator_source',
-			outputs = {natural_gas_global_bus: solph.Flow(variable_costs = 0)} 
+			outputs = {natural_gas_global_bus: solph.Flow(variable_costs = 10)} 
    )
 energysystem.add(natural_gas_generator_source) 
 
   
 ##########################################################################################################################
-back_electricity_tr = solph.components.Transformer (
+back_electricity_tr = solph.components.Source (
     label = 'back_electricity_tr',
-		inputs = {natural_gas_global_bus: solph.Flow()},
 		outputs = {electricity_bus: solph.Flow(nominal_value = 10000, variable_costs = 999999)},
 		
  ) 
 energysystem.add(back_electricity_tr)
  
-back_heat_water_tr = solph.components.Transformer (
-    label = 'back_electricity_tr',
-		inputs = {natural_gas_global_bus: solph.Flow()},
+back_heat_water_tr = solph.components.Source (
+    label = 'back_heat_water_tr',
 		outputs = {heat_water_bus: solph.Flow(nominal_value = 10000, variable_costs = 999999)},
 		
  ) 
 energysystem.add(back_heat_water_tr)
  
 
-back_steam_tr = solph.components.Transformer (
+back_steam_tr = solph.components.Source (
     label = 'back_steam_tr',
-		inputs = {natural_gas_global_bus: solph.Flow()},
 		outputs = {steam_bus: solph.Flow(nominal_value = 10000, variable_costs = 999999)},
 		
  ) 
 energysystem.add(back_steam_tr)
 ##########################################################################################################################
 
-
-#  1 т/ч = 0,627 МВт
-# пт-60: el.power = 60, heat_water = 151 при расходе 220*0,627 = 137.94   steam = 210*1.163 = 244.23 при расходе  360*0,627 = 225.72
-
-
+ 
 ##########################################################################################################################
 
-natural_gas_pt_60_bus = solph.Bus('natural_gas_pt_60_bus')
-electricity_pt_60_bus = solph.Bus('electricity_pt_60_bus')
-fake_on_of_pt_60_bus = solph.Bus('fake_on_of_pt_60_bus')
-heat_water_chp_bus = solph.Bus('heat_water_chp_bus')
-steam_chp_bus = solph.Bus('steam_chp_bus')
+
+chp_pt_60_gas_inner_general_bus = solph.Bus('chp_pt_60_gas_inner_general_bus')
+chp_pt_60_gas_inner_P_bus = solph.Bus('chp_pt_60_gas_inner_P_bus')
+chp_pt_60_on_of_bus = solph.Bus('chp_pt_60_on_of_bus')
+chp_pt_60_heat_water_inner_chp_bus = solph.Bus('chp_pt_60_heat_water_inner_chp_bus')
+chp_pt_60_steam_inner_bus = solph.Bus('chp_pt_60_steam_inner_bus')
+chp_pt_60_electricity_inner_bus = solph.Bus('chp_pt_60_electricity_bus')
+chp_pt_60_electricity_main_output_bus = solph.Bus('chp_pt_60_electricity_main_output_bus')
+
+energysystem.add(chp_pt_60_gas_inner_general_bus, chp_pt_60_gas_inner_P_bus)
+energysystem.add(chp_pt_60_on_of_bus, chp_pt_60_heat_water_inner_chp_bus )
+energysystem.add(chp_pt_60_steam_inner_bus, chp_pt_60_electricity_inner_bus)
+energysystem.add(chp_pt_60_electricity_main_output_bus)
+
+# chp_pt_60_heat_water_main_output_bus = solph.Bus('chp_pt_60_heat_water_main_output_bus')
+# chp_pt_60_steam_main_output_bus = solph.Bus('chp_pt_60_steam_main_output_bus')
 
 
-
-chp_pt_60_natural_gas_tr = solph.components.Transformer (
-    label = 'chp_pt_60_natural_gas_tr',
+chp_pt_60_gas_inner_general_tr = solph.components.Transformer (
+    label = 'chp_pt_60_gas_inner_general_tr',
 		inputs = {natural_gas_global_bus: solph.Flow()},
-		outputs = {natural_gas_pt_60_bus: solph.Flow(nominal_value = 3000)},
-		
+		outputs = {chp_pt_60_gas_inner_general_bus: solph.Flow(nominal_value = 198.36)},
+		conversion_factors = {natural_gas_global_bus:1,chp_pt_60_gas_inner_general_bus:1}
+
+ ) 
+
+chp_pt_60_gas_inner_P_tr = solph.components.Transformer (
+    label = 'chp_pt_60_gas_inner_P_tr',
+		inputs = {natural_gas_global_bus: solph.Flow()},
+		outputs = {chp_pt_60_gas_inner_P_bus: solph.Flow(nominal_value = 107.02)},
+		conversion_factors = {natural_gas_global_bus:1,chp_pt_60_gas_inner_P_bus:1}
  ) 
 
 chp_pt_60_P_mode_first_tr = solph.components.Transformer (
-    label = 'chp_pt_60_P_mode_tr',
-		inputs = {natural_gas_pt_60_bus: solph.Flow(nominal_value = 1000)},
-		outputs = {electricity_pt_60_bus: solph.Flow(nominal_value = 60),
-								fake_on_of_pt_60_bus: solph.Flow(),
-								steam_chp_bus: solph.Flow(nominal_value = 244.23)
+    label = 'chp_pt_60_P_mode_first_tr',
+		inputs = {chp_pt_60_gas_inner_general_bus: solph.Flow()},
+		outputs = {electricity_bus: solph.Flow(),
+								chp_pt_60_on_of_bus: solph.Flow(),
+								steam_bus: solph.Flow()
                },
-		conversion_factors = {natural_gas_pt_60_bus: 6.76, electricity_pt_60_bus: 1, steam_chp_bus: 4.07, fake_on_of_pt_60_bus: 5}
- ) 
-
-chp_pt_60_P_mode_second_tr = solph.components.Transformer (
-    label = 'chp_pt_60_P_mode_second_tr',
-		inputs = {natural_gas_pt_60_bus: solph.Flow(nominal_value = 500),
-							fake_on_of_pt_60_bus: solph.Flow()
-            },
-		outputs = {electricity_pt_60_bus: solph.Flow(nominal_value = 60),
-								steam_chp_bus: solph.Flow(nominal_value = 244.23)
-               },
-		conversion_factors = {fake_on_of_pt_60_bus: 0.0001,natural_gas_pt_60_bus: 6.76, electricity_pt_60_bus: 1, steam_chp_bus: 4.07}
+		conversion_factors = {chp_pt_60_gas_inner_general_bus: (1 + 3.8) / 0.91, electricity_bus: 1, steam_bus: 3.8, chp_pt_60_on_of_bus: 100}
  ) 
 
 chp_pt_60_T_mode_tr = solph.components.Transformer (
     label = 'chp_pt_60_T_mode_tr',
-		inputs = {natural_gas_pt_60_bus: solph.Flow( nominal_value = 1000)},
-		outputs = {electricity_pt_60_bus: solph.Flow(nominal_value = 60),
-								heat_water_chp_bus: solph.Flow(nominal_value = 12)
+		inputs = {chp_pt_60_gas_inner_general_bus: solph.Flow()},
+		outputs = {heat_water_bus: solph.Flow(),
+								electricity_bus: solph.Flow()
              },
-  	conversion_factors = {natural_gas_pt_60_bus: 4, electricity_pt_60_bus: 1, steam_chp_bus: 2}
+  	conversion_factors = {chp_pt_60_gas_inner_general_bus: (1 + 2.02) / 0.91, electricity_bus: 1, heat_water_bus: 2.02}
 		
  ) 
 
-
-chp_electric_output_tr = solph.components.Transformer (
-    label = 'chp_electric_output_tr',
-		inputs = {electricity_pt_60_bus: solph.Flow(),
-							steam_chp_bus: solph.Flow(),
-							heat_water_chp_bus:solph.Flow()
+chp_pt_60_P_mode_second_tr = solph.components.Transformer (
+    label = 'chp_pt_60_P_mode_second_tr',
+		inputs = {chp_pt_60_gas_inner_P_bus: solph.Flow(),
+							chp_pt_60_on_of_bus: solph.Flow()
             },
-		outputs = {electricity_bus: solph.Flow(nominal_value = 60, min = 0.3, nonconvex = solph.NonConvex()),
-							steam_chp_bus: solph.Flow(),
-							heat_water_chp_bus:solph.Flow()
-             },
+		outputs = {electricity_bus: solph.Flow(),
+								steam_bus: solph.Flow()
+               },
+		conversion_factors = {chp_pt_60_on_of_bus: 0.0001, chp_pt_60_gas_inner_P_bus: (1 + 3.8) / 0.91, electricity_bus: 1, steam_bus: 3.8}
  ) 
 
+# chp_pt_60_electricity_main_output_tr = solph.components.Transformer (
+#     label = 'chp_pt_60_electricity_main_output_tr',
+# 		inputs = {chp_pt_60_electricity_inner_bus: solph.Flow(),
+# 							chp_pt_60_heat_water_inner_chp_bus:solph.Flow(),
+# 							chp_pt_60_steam_inner_bus: solph.Flow(),
+#             },
+# 		outputs = {electricity_bus: solph.Flow(nominal_value = 60),
+# 							# heat_water_bus: solph.Flow(),
+# 							# steam_bus:solph.Flow()
+#              },
+#  ) 
 
-energysystem.add(chp_pt_60_natural_gas_tr, chp_pt_60_P_mode_first_tr, chp_pt_60_P_mode_second_tr, chp_electric_output_tr)
 
-
+energysystem.add(chp_pt_60_gas_inner_general_tr, chp_pt_60_gas_inner_P_tr)
+energysystem.add(chp_pt_60_P_mode_first_tr, chp_pt_60_T_mode_tr)
+energysystem.add(chp_pt_60_P_mode_second_tr)
 
 
 
@@ -186,25 +196,42 @@ gas_data = solph.views.node(results, "natural_gas_global_bus")["sequences"].drop
 
 
 electr_df = pd.DataFrame()
-electr_df['ТЭЦ-электроэнергия'] = electricity_data[((back_chp_electricity_tr.label, electricity_bus.label),'flow')]
+electr_df['ПТ-60_электроэнергия-теплофикац.часть'] = electricity_data[((chp_pt_60_T_mode_tr.label, electricity_bus.label),'flow')]
+electr_df['ПТ-60_электроэнергия-промышленнвя 1'] = electricity_data[((chp_pt_60_P_mode_first_tr.label, electricity_bus.label),'flow')]
+electr_df['ПТ-60_электроэнергия-промышленная 2'] = electricity_data[((chp_pt_60_P_mode_second_tr.label, electricity_bus.label),'flow')]
 # electr_df['Дорогой генератор'] = electricity_data[((back_electricity_tr.label, electricity_bus.label),'flow')]
 
+print(electr_df['ПТ-60_электроэнергия-промышленнвя 1']) 
+
+
 gas_df = pd.DataFrame()
-gas_df['Расход газа ТЭЦ'] = gas_data[((natural_gas_global_bus.label, back_chp_electricity_tr.label),'flow')]
+gas_df['Расход газа ПТ-60 - П - 1 часть  '] = gas_data[((natural_gas_global_bus.label, chp_pt_60_gas_inner_general_tr.label),'flow')]
+gas_df['Расход газа ПТ-60 - П - 2 часть '] = gas_data[((natural_gas_global_bus.label, chp_pt_60_gas_inner_P_tr.label),'flow')]
+
+
+
 
 heat_water_df = pd.DataFrame()
-heat_water_df['ТЭЦ-горячая вода'] = heat_water_data[((back_chp_electricity_tr.label, heat_water_bus.label),'flow')]
+heat_water_df['ТЭЦ-горячая вода'] = heat_water_data[((chp_pt_60_T_mode_tr.label, heat_water_bus.label),'flow')]
+
+
+steam_data_df = pd.DataFrame()
+steam_data_df['ТЭЦ-пар-1'] = steam_data[((chp_pt_60_P_mode_first_tr.label, steam_bus.label),'flow')]
+steam_data_df['ТЭЦ-пар-2'] = steam_data[((chp_pt_60_P_mode_second_tr.label, steam_bus.label),'flow')]
+
+
 
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 
 
+maxY = 1000;
 
 
-
-ax1 = electr_df.plot(kind="area", ylim=(0, 3000), ax = axes[0,1]  ,legend = 'reverse', title = 'Генерация электроэнергии')
-ax2 = heat_water_df.plot(kind="area", ylim=(0, 3000) , ax = axes[1,0] ,  legend = 'reverse', title = 'Генерация тепла')
-ax3 = gas_df.plot(kind="area", ylim=(0, 3000) , ax = axes[0, 0] ,  legend = 'reverse', title = 'Расход газа ТЭЦ')
+ax1 = electr_df.plot(kind="area", ylim=(0, maxY), ax = axes[0,1]  ,legend = 'reverse', title = '"Электроэнергия ПТ-60"')
+ax2 = heat_water_df.plot(kind="area", ylim=(0, maxY) , ax = axes[1,1] ,  legend = 'reverse', title = 'Горячая вода ПТ-60')
+ax2 = steam_data_df.plot(kind="area", ylim=(0, maxY) , ax = axes[1,0] ,  legend = 'reverse', title = 'Пар ПТ-60')
+ax3 = gas_df.plot(kind="area", ylim=(0, maxY) , ax = axes[0, 0] ,  legend = 'reverse', title = 'Расход газа ПТ-60')
 
 
 plt.show()
